4ca175f49c40677ebb02af58f4390e1e5473e382
oukpov <112316490+oukpov@users.noreply.github.com>
2022-11-23 17:31:20 +0700

    dra1

--- lib/action/delete.dart
+++ lib/action/delete.dart
@@ -1,11 +1,10 @@
-
 import 'package:getwidget/components/button/gf_button.dart';
 import 'package:getwidget/shape/gf_button_shape.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:pdf/pdf.dart';
-import 'package:printing/printing.dart';
+
 class DeleteId extends StatefulWidget {
   const DeleteId({super.key});
 
@@ -18,25 +17,25 @@ class _DeleteIdState extends State<DeleteId> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-       child: GFButton(
-                                shape: GFButtonShape.pills,
-                                color: Colors.blue,
-                                onPressed: () {
-                                  setState(() {
-                                    generatePdf();
-                                    //print("go");
-                                  });
-                                },
-                                text: 'Print',
-                                icon: const Icon(Icons.print),
-                              ),
+        child: GFButton(
+          shape: GFButtonShape.pills,
+          color: Colors.blue,
+          onPressed: () {
+            setState(() {
+              // generatePdf();
+              //print("go");
+            });
+          },
+          text: 'Print',
+          icon: const Icon(Icons.print),
+        ),
       ),
     );
   }
 
-Future<Uint8List> _generatePdf(PdfPageFormat format, String title) async {
+  Future<Uint8List> _generatePdf(PdfPageFormat format, String title) async {
     final pdf = pw.Document(version: PdfVersion.pdf_1_5, compress: true);
-    final font = await PdfGoogleFonts.nunitoExtraLight();
+    // final font = await PdfGoogleFonts.nunitoExtraLight();
     final ByteData bytes = await rootBundle.load('assets/images/KFA-Logo.png');
     final Uint8List byteList = bytes.buffer.asUint8List();
     pdf.addPage(pw.Page(build: (context) {
@@ -52,31 +51,25 @@ Future<Uint8List> _generatePdf(PdfPageFormat format, String title) async {
               fit: pw.BoxFit.fill),
         ),
         pw.Container(
-          alignment: pw.Alignment.center,
-          height: 50,
-          // width: 150,
-          child: pw.Text('Check Verbal',
-          //style: TextStyle(color: Colors.amber,fontSize: 15),
-          )
-        ),
+            alignment: pw.Alignment.center,
+            height: 50,
+            // width: 150,
+            child: pw.Text(
+              'Check Verbal',
+              //style: TextStyle(color: Colors.amber,fontSize: 15),
+            )),
         pw.Container(
-          alignment: pw.Alignment.centerRight,
-          height: 50,
-          child: pw.Table(
-            //border: TableBorder.all();
-            children: [
-              
-            ]
-            )
-        )
-       // )
+            alignment: pw.Alignment.centerRight, height: 50, child: pw.Table(
+                //border: TableBorder.all();
+                children: []))
+        // )
       ]);
     }));
     return pdf.save();
   }
 
-  void generatePdf() async {
-    const title = 'Flutter Demo';
-    await Printing.layoutPdf(onLayout: (format) => _generatePdf(format, title));
-  }
+  // void generatePdf() async {
+  //   const title = 'Flutter Demo';
+  //   await Printing.layoutPdf(onLayout: (format) => _generatePdf(format, title));
+  // }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:admin/device/mobile/AutoVerbal/AutoVerbal.dart';
 import 'package:admin/device/mobile/comparable/Comparable_list_view.dart';
 import 'package:admin/device/mobile/on_home_page.dart';
+import 'package:admin/interface/homepage.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:get/get.dart';
-
-=======
-import 'package:fvm/fvm.dart';
->>>>>>> 0203833afdc6c8a3127ff772e565ef3d3d4497b2
-import 'interface/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,19 +16,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-
-<<<<<<< HEAD
-      home: const Hompage(),
-
-      // home: Show_autoVerbals(),
-=======
-      home: Hompage(),
->>>>>>> 0203833afdc6c8a3127ff772e565ef3d3d4497b2
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Hompage());
   }
 }
