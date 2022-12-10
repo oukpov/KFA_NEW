f2851b3d1dd06cd271450a41b28d4596cedfde7a
Simeng <ponloksimeng@gmail.com>
2022-12-10 10:45:35 +0700

    ff

---
+++ lib/device/mobile/navigate_setting/auto/menu_auto.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class ManuAuto extends StatefulWidget {
+  const ManuAuto({super.key});
+
+  @override
+  State<ManuAuto> createState() => _ManuAutoState();
+}
+
+class _ManuAutoState extends State<ManuAuto> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
